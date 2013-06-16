3ef87fc80d05baf3d51b81a22ac160355466f169
Nathan Staines <nathan@nathanstaines.com>
2013-06-16 20:38:05 +0100

    Round twelve

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -15,10 +15,12 @@ require.config({
     'backbone-relational': ['backbone']<% } %>
   },
   paths: {
-    'jquery': '../components/jquery/jquery',
+    'jquery': '../components/jquery/jquery',<% if (includeBackboneRelational) { %>
+    'backbone': '../components/backbone/backbone',
+    'underscore': '../components/underscore/underscore',
+    'backbone-relational': '../components/backbone-relational/backbone-relational'<% }else{ %>
     'backbone': '../components/backbone-amd/backbone',
-    'underscore': '../components/underscore-amd/underscore'<% if (includeBackboneRelational) { %>,
-    'backbone-relational': '../components/backbone-relational/backbone-relational'<% } %>
+    'underscore': '../components/underscore-amd/underscore'<% } %>
   }
 });
 
