c7893728679c30370a0a8f88c715766df934629c
Nathan Staines <nathan@nathanstaines.com>
2013-07-06 20:24:08 +0100

    Add a few comments and remove some whitespace

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,9 +1,14 @@
+'use strict';
+
+// Module dependencies
 var express = require('express'),
     http = require('http'),
     path = require('path');
 
+// Create server
 var app = express();
 
+// Configure server
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -18,6 +23,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Start server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
